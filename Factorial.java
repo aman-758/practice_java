6709b777a3eefaf30cea60edd06e626c52f9c28c
Aman <amansaxenasaxena759@gmail.com>
2022-08-08 15:45:30 +0530

    Adding comment in my code

--- Factorial.java
+++ Factorial.java
@@ -2,6 +2,7 @@ package basicPrograms;
 
 public class Factorial {
 	public static void main(String[] args) {
+		// THIS PROGRAM WILL SHOW YOU THE FACTORIAL OF NUMBER 5
 		int fact = 1;
 		for(int i = 1;i<=5;i++) {
 			fact=fact*i; //0 = 1,2,6,24,5 = 120 
